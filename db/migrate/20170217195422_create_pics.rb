a85274dbc62b79d8606efd7705caf4d67c94488f
A.Espinozaflores <a.espinozaflores@hotmail.com>
2017-02-18 13:45:24 -0600

    second commit

---
+++ db/migrate/20170217195422_create_pics.rb
@@ -0,0 +1,10 @@
+class CreatePics < ActiveRecord::Migration[5.0]
+  def change
+    create_table :pics do |t|
+      t.string :photo
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
